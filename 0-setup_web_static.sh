c1119e1ebbcb3bba003fd9040c9ba8252b4ccf19
dene009 <esidene009@gmail.com>
2023-04-18 06:03:59 -0700

    fixing error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,18 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# sets up the web servers for the deployment of web_static. 
 
-apt-get update
-apt-get install -y nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
 
-service nginx restart
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
